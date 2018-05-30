4bc8182321b8a53a9c7db3be0bda0f019175058d
Edison Li <li2343@purdue.edu>
2018-05-30 08:23:38 +0800

    Fixed a bug

--- src/SpacePort.java
+++ src/SpacePort.java
@@ -1,7 +1,8 @@
 import java.util.List;
 
 public class SpacePort extends Infrastructure {
-    SpacePort(double defense, double strength, List<Technology> technologies, double energyUsage, int technologyLevel) {
-        super(defense, strength, technologies, energyUsage, technologyLevel);
+
+    SpacePort(double defense, double strength, List<Technology> technologies, double energyUsage, int technologyLevel, double workers) {
+        super(defense, strength, technologies, energyUsage, technologyLevel, workers);
     }
 }
